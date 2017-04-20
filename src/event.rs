c7b2f53291ffb0c2e318f600fbe6dfaa775a871a
Eugene Bulkin <eugene.bulkin2@gmail.com>
2017-04-20 16:05:29 -0500

    Add missing docs for play descriptions.

--- src/event.rs
+++ src/event.rs
@@ -328,17 +328,25 @@ pub enum PlayDescription {
     /// A batter lining into a double play. Should be followed with an `LDP` modifier, but we don't
     /// verify this.
     LinedIntoDoublePlay {
+        /// The fielder who made the first out.
         first_out: Fielder,
+        /// The throws for the second out.
         second_out: Vec<Fielder>,
+        /// The base the second out was recorded on.
         second_out_runner: Base,
     },
     /// A batter lining into a triple play. Should be followed with an `LTP` modifier, but we don't
     /// verify this.
     LinedIntoTriplePlay {
+        /// The fielder who made the first out.
         first_out: Fielder,
+        /// The throws for the second out.
         second_out: Vec<Fielder>,
+        /// The base the second out was recorded on.
         second_out_runner: Base,
+        /// The throws for the third out.
         third_out: Vec<Fielder>,
+        /// The base the third out was recorded on.
         third_out_runner: Base,
     }
     // TODO: Finish all of these!
